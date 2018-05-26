62255a50499c95bbdbedc0fb24509b78b6c09194
Mangesh Tamhankar <mtamhankar@idatainc.com>
2018-05-26 09:51:38 -0400

    make some squares

--- src/Board.js
+++ src/Board.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import Square from './Square'
 
 class Board extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = { starting position for chess }
+  // }
+
   render() {
+    const squares = []
+    for (let j = 0; j < 8; j++) {
+      squares.push(<Square key={j}/>)
+    }
     return (
       <div>
         <h2>Board Placeholder</h2>
         <div>I have many <Square/></div>
+        <div>Let's actually make some</div>
+        <div className="board">{squares}</div>
       </div>
     )
   }
--- src/Square.js
+++ src/Square.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class Square extends Component {
   render() {
     return (
-      <div>
-        <h3>Square Placeholder</h3>
-      </div>
+      <div className="square"></div>
     )
   }
 }
