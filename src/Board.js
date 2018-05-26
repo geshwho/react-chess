a947afa6959e5c16ad4b382d41592ab4dd34ffa0
Mangesh Tamhankar <mtamhankar@idatainc.com>
2018-05-26 09:35:35 -0400

    placeholders and stuff

---
+++ src/Board.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Square from './Square'
+
+class Board extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Board Placeholder</h2>
+        <div>I have many <Square/></div>
+      </div>
+    )
+  }
+}
+
+export default Board;
---
+++ src/Square.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Square Placeholder</h3>
+      </div>
+    )
+  }
+}
+
+export default Square;
