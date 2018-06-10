720c1a5e337871cd2dbcd0db882412bc52aa5f97
Mangesh Tamhankar <mtamhankar@idatainc.com>
2018-06-10 09:18:05 -0400

    draw a board

--- src/Board.js
+++ src/Board.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import Square from './Square'
 
 class Board extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = { starting position for chess }
-  // }
+  constructor() {
+    super();
+    // We'll use FEN notation. Cuz.. why re-invent something?
+    // Also chess.js uses this if I decide this is only going to be a UI project
+    // and chess.js will handle all of the validation, moving pieces, etc.
+    this.state = {
+      position: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    }
+  }
+
+  replacer(match, offset, string) {
+    const rep = parseInt(match,10);
+    return ' '.repeat(rep);
+  }
 
   render() {
     const squares = []
+    var pieces = this.state.position.split(' ')[0];
+    pieces = pieces.replace(/\d/g, this.replacer);
+    pieces = pieces.split('/');
     for (let i = 0; i < 8; i++) {
       const col = []
       for (let j = 0; j < 8; j++) {
-        col.push(<Square key={j}/>)
+        col.push(<Square key={j} piece={pieces[i][j]}/>)
       }
       squares.push(<div className="col" key={i}>{col}</div>)
     }
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,78 @@
 import React, { Component } from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import {  faChessPawn,
+          faChessKnight,
+          faChessBishop,
+          faChessRook,
+          faChessQueen,
+          faChessKing
+        } from '@fortawesome/fontawesome-free-solid/'
 
 class Square extends Component {
   render() {
+    var ico = null;
+    var color = null
+    switch(this.props.piece) {
+      case 'P':
+        ico = faChessPawn;
+        break;
+      case 'N':
+        ico = faChessKnight;
+        break;
+      case 'B':
+        ico = faChessBishop;
+        break;
+      case 'R':
+        ico = faChessRook;
+        break;
+      case 'Q':
+        ico = faChessQueen;
+        break;
+      case 'K':
+        ico = faChessKing;
+        break;
+      case 'p':
+        ico = faChessPawn;
+        break;
+      case 'n':
+        ico = faChessKnight;
+        break;
+      case 'b':
+        ico = faChessBishop;
+        break;
+      case 'r':
+        ico = faChessRook;
+        break;
+      case 'q':
+        ico = faChessQueen;
+        break;
+      case 'k':
+        ico = faChessKing;
+        break;
+    }
+    switch(this.props.piece) {
+      case 'P':
+      case 'N':
+      case 'B':
+      case 'R':
+      case 'Q':
+      case 'K':
+        color = 'white';
+        break;
+      case 'p':
+      case 'n':
+      case 'b':
+      case 'r':
+      case 'q':
+      case 'k':
+        color = 'black';
+        break;
+    }
+    const piece = ico ? <FontAwesomeIcon icon={ico} size="2x" color={color} className="piece"/> : null;
     return (
-      <div className="square"></div>
+      <div className="square">
+        {piece}
+      </div>
     )
   }
 }
