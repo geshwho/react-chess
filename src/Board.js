6213ad08ccef7965e0a8534461c903fa580bf0d3
Mangesh Tamhankar <mtamhankar@idatainc.com>
2018-07-15 11:33:14 -0400

    Add chess.js for all the fancy needs

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Square from './Square'
 import {toRowCol, toAlgebraicNotation} from './chess_notation'
+import Chess from 'chess.js'
 
 class Board extends Component {
   constructor() {
@@ -10,18 +11,22 @@ class Board extends Component {
       position: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
       selectedSquare: null,
       row: null,
-      col: null
+      col: null,
+      chess: new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
     }
 
     this.squareSelector = this.squareSelector.bind(this);
   }
 
   squareSelector(square) {
-    this.setState({
-      selectedSquare: toAlgebraicNotation(square.row, square.col),
-      row: square.row,
-      col: square.col,
-    });
+    const algebraicSquare = toAlgebraicNotation(square.row, square.col)
+    if(this.state.chess.get(algebraicSquare) !== null){
+      this.setState({
+          selectedSquare: algebraicSquare,
+        row: square.row,
+        col: square.col,
+      });
+    }
   }
 
   replacer(match, offset, string) {
@@ -29,24 +34,32 @@ class Board extends Component {
     return ' '.repeat(rep);
   }
 
-  render() {
-    // TODO: move logic outside of render
-    const squares = []
+  getSquares() {
+    // Converts FEN into <Square>s
+    const squares = [];
+    const moves = this.state.chess.moves({ square: this.state.selectedSquare });
+    console.log(moves);
     var pieces = this.state.position.split(' ')[0];
     pieces = pieces.replace(/\d/g, this.replacer);
     pieces = pieces.split('/');
     for (let i = 0; i < 8; i++) {
       const col = []
       for (let j = 0; j < 8; j++) {
-        col.push(<Square key={j} row={i} col={j} piece={pieces[i][j]} selector={this.squareSelector} selected={this.state.row===i && this.state.col===j} />)
+        const selected = this.state.row===i && this.state.col===j;
+        col.push(<Square key={j} row={i} col={j} piece={pieces[i][j]} selector={this.squareSelector} selected={selected} />)
       }
       squares.push(<div className="col" key={i}>{col}</div>)
     }
+    return squares;
+  }
+
+  render() {
+    const sqs = this.getSquares();
     return (
       <div>
         <h2>Board Placeholder</h2>
         <div>Let's actually make some squares</div>
-        <div className="board">{squares}</div>
+        <div className="board">{sqs}</div>
       </div>
     )
   }
